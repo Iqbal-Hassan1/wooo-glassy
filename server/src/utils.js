2d8ca1cc79f25f5580c133702c1084114639f9a6
Iqbal Hassan <engineeriqbal2584@gmail.com>
2022-07-07 16:31:34 +0500

    complete project setting

---
+++ server/src/utils.js
@@ -0,0 +1,9 @@
+/**
+ * Clones the given data.
+ */
+exports.clone = function (data, defaultValue) {
+    if (typeof data === 'undefined')
+        return defaultValue;
+
+    return JSON.parse(JSON.stringify(data));
+};
